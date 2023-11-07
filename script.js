f4e9a256dac6235407374ac156be73788bd9e50c
i2305 <iva.vukovic695@gmail.com>
2023-11-07 09:31:34 +0100

    switch constructors for classes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const myLibrary=[];
 
-function Book (title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-} 
+class Book {
+    constructor(title, author, pages, read){
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.read=read;
+    }
+}
 
 Book.prototype.toggleRead=function(){
     this.read = !this.read;
     
 }
+
 
 function toggleRead(index){
     myLibrary[index].toggleRead();
